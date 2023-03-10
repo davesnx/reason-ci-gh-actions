5f004121253305dba0ceaaa1533bd1a94bb58320
David Sancho Moreno <dsnxmoreno@gmail.com>
2023-03-10 12:34:19 +0100

    Add debug cache

--- .github/workflows/print-esy-cache-debug.js
+++ .github/workflows/print-esy-cache-debug.js
@@ -6,9 +6,14 @@ const ESY_FOLDER = process.env.ESY__PREFIX
   ? process.env.ESY__PREFIX
   : path.join(os.homedir(), ".esy");
 
+console.log("process.env.ESY__PREFIX", process.env.ESY__PREFIX);
+console.log("path.join(os.homedir()", path.join(os.homedir(), ".esy"));
+
 const someEsy3 = fs
   .readdirSync(ESY_FOLDER)
   .filter((name) => name.length > 0 && name[0] === "3");
+
+console.log("someEsy3", someEsy3)
 
 const esy3 = someEsy3
   .sort()
